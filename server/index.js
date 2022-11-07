70168f259d6aa6e764ec1f12f56d26d00dba9c6b
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2022-11-07 13:28:35 -0500

    Add 3rd party API fetch request in server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': process.env.WORDSAPI_KEY,
+            'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://wordsapiv1.p.rapidapi.com/words/.22-caliber/pertainsTo', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
     res.send("apple");
 });
 
