dd56d0c97d9e0515e0c8ddfff42762f4dfb7da5e
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2022-11-07 23:00:06 -0500

    Lay groundwork for server

--- server/index.js
+++ server/index.js
@@ -1,26 +1,30 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-const router = express.Router();
 
-router.get("/", function (req, res, next) {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': process.env.WORDSAPI_KEY,
-            'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://wordsapiv1.p.rapidapi.com/words/.22-caliber/pertainsTo', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    res.send("apple");
+
+require("dotenv").config();
+const axios = require("axios");
+
+app.get("/:word", (req, res) => {
+    console.log(req.params.word);
+    res.sendStatus(200);
 });
 
-app.use("/api/", router);
+// const word = "popsicle";
+// const options = {
+//   method: 'GET',
+//   url: `https://wordsapiv1.p.rapidapi.com/words/${word}/syllables`,
+//   headers: {
+//     'X-RapidAPI-Key': process.env.WORDSAPI_KEY,
+//     'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+//   }
+// };
 
-const server = app.listen(5000, function() {
-    console.log("Node Server is listening on port 5000");
-});
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+
+
+app.listen(5001);
